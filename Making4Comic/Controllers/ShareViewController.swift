6cac3e73061ec6716a50ac94b1c43395db0b625d
原田茂大 <sapori946@gmail.com>
2019-12-29 22:58:38 +0800

    no message

---
+++ Making4Comic/Controllers/ShareViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ShareViewController.swift
+//  Making4Comic
+//
+//  Created by 原田茂大 on 2019/12/29.
+//  Copyright © 2019 geshi. All rights reserved.
+//
+
+import UIKit
+
+class ShareViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
